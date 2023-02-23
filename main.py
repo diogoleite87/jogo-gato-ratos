664adeaeecdca322310fbdd52444b4153c0e93c7
Diogo Leite <diogoleite87@gmail.com>
2023-02-22 23:03:51 -0300

    feat: add minimax IA and auxiliary functions

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from os import system
+from random import choice
 import re
 import copy
+from math import inf as infinity
+import platform
 
 HUMANO = -1
-COMPUTADOR = +1
+COMP = +1
+jogada_inicial = True
 
 tabuleiro = [
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -16,11 +21,11 @@ tabuleiro = [
 ]
 
 
-def imprimeTabuleiro():
+def imprimeTabuleiro(estado):
 
     # imprime o tabuleiro na tela
 
-    for row in tabuleiro:
+    for row in estado:
         print('\n--------------------------------------------------------------------------------')
         for cell in row:
             if cell == +1:
@@ -32,34 +37,31 @@ def imprimeTabuleiro():
     print('\n--------------------------------------------------------------------------------')
 
 
-def verificaVitoriaRato():
+def verificaVitoriaRato(estado):
 
     # verifica se o rato(computador) venceu por um deles chegar do outro lado
 
-    for i in range(len(tabuleiro)):
-        if tabuleiro[7][i] == 1:
+    for i in range(len(estado)):
+        if estado[7][i] == 1:
             return True
 
     # verifica se o rato(computador) venceu por ter eliminado o gato
 
     gato_eliminado = True
 
-    for i in tabuleiro:
+    for i in estado:
         for j in i:
             if j == -1:
                 gato_eliminado = False
 
-    if gato_eliminado:
-        return True
-
-    return False
+    return gato_eliminado
 
 
-def verificaVitoriaGato():
+def verificaVitoriaGato(estado):
 
     # verifica se o gato(humano) venceu por ter eliminado todos os ratos
 
-    for i in tabuleiro:
+    for i in estado:
         for j in i:
             if j == 1:
                 return False
@@ -194,11 +196,212 @@ def movimentaGato():
             print('\nMovimento Invalido!')
 
 
+def localizacaoRatos(estado):
+
+    lista_localizacao = []
+
+    for x in range(len(estado)):
+        for y in range(len(estado)):
+            if estado[x][y] == 1:
+                lista_localizacao.append([x, y])
+
+    return lista_localizacao
+
+
+def movimentosValidosRatos(estado):
+
+    lista_localizacao = localizacaoRatos(estado)
+    lista_movimentos = []
+
+    for item in lista_localizacao:
+
+        if estado[item[0] + 1][item[1]] == -1:
+            continue
+        elif item[0] == 1:
+            lista_movimentos.append([item, [item[0] + 1, item[1]]])
+            lista_movimentos.append([item, [item[0] + 2, item[1]]])
+        elif item[0] == 7:
+            continue
+        else:
+            lista_movimentos.append([item, [item[0] + 1, item[1]]])
+
+        if item[0] - 1 >= 0 and item[1] + 1 <= 7:
+            if estado[item[0] - 1][item[1] + 1] == -1:
+                lista_movimentos.append([item, [item[0] - 1, item[1] + 1]])
+        if item[0] - 1 >= 0 and item[1] - 1 >= 0:
+            if estado[item[0] - 1][item[1] - 1] == -1:
+                lista_movimentos.append([item, [item[0] - 1, item[1] - 1]])
+        if item[0] + 1 <= 7 and item[1] + 1 <= 7:
+            if estado[item[0] + 1][item[1] + 1] == -1:
+                lista_movimentos.append([item, [item[0] + 1, item[1] + 1]])
+        if item[0] + 1 <= 7 and item[1] - 1 >= 0:
+            if estado[item[0] + 1][item[1] - 1] == -1:
+                lista_movimentos.append([item, [item[0] + 1, item[1] - 1]])
+
+    return lista_movimentos
+
+
+def movimentosValidosGato(estado):
+
+    posicao_gato = []
+    lista_movimentos = []
+
+    for i in range(len(estado)):
+        for j in range(len(estado)):
+            if estado[i][j] == -1:
+                posicao_gato = [i, j]
+
+    for i in range(len(estado)):
+        if posicao_gato[1] == i:
+            continue
+        else:
+            lista_movimentos.append([posicao_gato, [posicao_gato[0], i]])
+
+    for i in range(len(estado)):
+        if posicao_gato[0] == i:
+            continue
+        else:
+            lista_movimentos.append([posicao_gato, [i, posicao_gato[1]]])
+
+    return lista_movimentos
+
+
+def posicaoGato(estado):
+    posicao_gato = []
+
+    for i in range(len(estado)):
+        for j in range(len(estado)):
+            if estado[i][j] == -1:
+                posicao_gato = [i, j]
+
+    return posicao_gato
+
+
+def avaliacao(estado):
+    if verificaVitoriaRato(estado):
+        placar = +1
+    elif verificaVitoriaGato(estado):
+        placar = -1
+    else:
+        placar = 0
+
+    return placar
+
+
+def fimJogo(estado):
+    return verificaVitoriaGato(estado) or verificaVitoriaRato(estado)
+
+
+def limpaConsole():
+    os_name = platform.system().lower()
+    if 'windows' in os_name:
+        system('cls')
+    else:
+        system('clear')
+
+
+def retrocedeMovimentoRato(estado, jogada):
+    estado[jogada[1][0]][jogada[1][1]] = 0
+    estado[jogada[0][0]][jogada[0][1]] = 1
+
+    return estado
+
+
+def retrocedeMovimentoGato(estado, jogada):
+    estado[jogada[1][0]][jogada[1][1]] = 0
+    estado[jogada[0][0]][jogada[0][1]] = -1
+
+    return estado
+
+
+def execJogadaRato(estado, jogada):
+
+    estado[jogada[0][0]][jogada[0][1]] = 0
+    estado[jogada[1][0]][jogada[1][1]] = 1
+
+    return estado
+
+
+def execJogadaGato(estado, jogada):
+
+    estado[jogada[1][0]][jogada[1][1]] = -1
+    estado[jogada[0][0]][jogada[0][1]] = 0
+
+    return estado
+
+
+def minimax(estado, profundidade, jogador):
+
+    if avaliacao(estado) != 0 or profundidade == 0:
+        return avaliacao(estado), None
+
+    if jogador == COMP:
+        placar = -infinity
+        movimentosRatos = movimentosValidosRatos(estado)
+        melhorJogada = None
+
+        for jogada in movimentosRatos:
+            execJogadaRato(estado, jogada)
+            placarAtual, _ = minimax(estado, profundidade - 1, HUMANO)
+            retrocedeMovimentoRato(estado, jogada)
+            if placarAtual > placar or melhorJogada is None:
+                placar = placarAtual
+                melhorJogada = jogada
+        return placar, melhorJogada
+    else:
+        placar = +infinity
+        movimentosGato = movimentosValidosGato(estado)
+        melhorJogada = None
+
+        for jogada in movimentosGato:
+            execJogadaGato(estado, jogada)
+            placarAtual, _ = minimax(estado, profundidade - 1, COMP)
+            retrocedeMovimentoGato(estado, jogada)
+            if placarAtual < placar or melhorJogada is None:
+                placar = placarAtual
+                melhorJogada = jogada
+
+        return placar, melhorJogada
+
+
+def movimentaRatoIA():
+
+    tabuleiro_minimax = copy.deepcopy(tabuleiro)
+
+    profundidade = 10
+
+    global jogada_inicial
+
+    if jogada_inicial:
+        jogada = choice(movimentosValidosRatos(tabuleiro))
+        execJogadaRato(tabuleiro, jogada)
+        jogada_inicial = False
+        return
+
+    print("Calculando movimentação rato...")
+
+    jogada = minimax(tabuleiro_minimax, profundidade, COMP)
+    execJogadaRato(tabuleiro, jogada[1])
+    return
+
+
 def main():
 
-    while(True):
-        # imprimeTabuleiro()
-        movimentaGato()
+    while(fimJogo(tabuleiro) == False):
+        imprimeTabuleiro(tabuleiro)
+        movimentaRatoIA()
+
+        if fimJogo(tabuleiro) == False:
+            movimentaGato()
+
+        limpaConsole()
+
+    if verificaVitoriaGato(tabuleiro):
+        imprimeTabuleiro(tabuleiro)
+        print("\nGato Venceu!")
+    else:
+        imprimeTabuleiro(tabuleiro)
+        print("\nRato Venceu!")
 
 
 if __name__ == '__main__':
